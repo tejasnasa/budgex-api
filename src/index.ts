635d6f946cc11af9555ce37c5ec52b5589cf2c83
tejasnasa <138145714+tejasnasa@users.noreply.github.com>
2024-11-09 18:23:31 +0530

    Deploy(5)

--- src/index.ts
+++ src/index.ts
@@ -9,8 +9,8 @@ app.get("/", (req, res) => {
   res.send("This is Budgex");
 });
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
-app.listen(port || 3000, () => {
+app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
