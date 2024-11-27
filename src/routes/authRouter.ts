934083b048bf711077d7459b622a235f898ae364
tejasnasa <138145714+tejasnasa@users.noreply.github.com>
2024-11-27 16:49:17 +0530

    Delete auth

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -1,11 +1,5 @@
 import express from "express";
-import login from "../controllers/auth/login";
-import register from "../controllers/auth/register";
 
 const authRouter = express.Router();
-
-authRouter.post("/login", login);
-authRouter.post("/register", register);
-authRouter.post("/googlelogin");
 
 export default authRouter;
